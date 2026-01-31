0216c5447bfb9c2ae25fbfe95f865cf435157c3c
souzza parabolica <cauelima752gmail.com>
2026-01-31 19:28:20 -0300

    Criação da seção de cardápios, links internos para o cardápio, criação da seção de feedbacks, correção de layout

--- script.js
+++ script.js
@@ -65,3 +65,84 @@ menuBtn.addEventListener("click", () => {
 
 // Fecha ao clicar em qualquer item
 mobileMenu.addEventListener("click", closeMenu);
+
+const btnBebidas = document.getElementById("btn-bebidas");
+const btnSalgadas = document.getElementById("btn-salgadas");
+const btnDoces = document.getElementById("btn-doces");
+const cardapio = document.querySelector(".cardapio div");
+const headerCardapio = document.getElementById("headerCardapio");
+
+btnSalgadas.addEventListener("click", () => {
+  lenis.scrollTo("#salgadas", {
+    duration: 1.2,
+    offset: -120, // ajuste fino se tiver header fixo
+    easing: (t) => 1 - Math.pow(1 - t, 3),
+  });
+});
+
+btnDoces.addEventListener("click", () => {
+  lenis.scrollTo("#doces", {
+    duration: 1.2,
+    offset: -20,
+    easing: (t) => 1 - Math.pow(1 - t, 3),
+  });
+});
+
+btnBebidas.addEventListener("click", () => {
+  lenis.scrollTo("#bebidas", {
+    duration: 1.2,
+    offset: -20,
+    easing: (t) => 1 - Math.pow(1 - t, 3),
+  });
+});
+
+const viewCardapio = document.getElementById("viewCardapio");
+const secaoCardapio = document.querySelector(".cardapio");
+
+viewCardapio.onclick = () => {
+  gsap.set(secaoCardapio, { display: "block" });
+  gsap.set(cardapio, { display: "flex" });
+
+  gsap.fromTo(
+    secaoCardapio,
+    { autoAlpha: 0, y: 30 },
+    {
+      autoAlpha: 1,
+      y: 0,
+      duration: 0.6,
+      ease: "power3.out",
+      onComplete: () => {
+        // 🔥 SCROLL CORRETO COM LENIS
+        lenis.scrollTo(secaoCardapio, {
+          offset: -80, // se tiver header fixo
+          duration: 1,
+          easing: (t) => 1 - Math.pow(1 - t, 3),
+        });
+      },
+    },
+  );
+};
+
+headerCardapio.onclick = () => {
+  gsap.set(secaoCardapio, { display: "block" });
+  gsap.set(cardapio, { display: "flex" });
+
+  gsap.fromTo(
+    secaoCardapio,
+    { autoAlpha: 0, y: 30 },
+    {
+      autoAlpha: 1,
+      y: 0,
+      duration: 0.6,
+      ease: "power3.out",
+      onComplete: () => {
+        // 🔥 SCROLL CORRETO COM LENIS
+        lenis.scrollTo(secaoCardapio, {
+          offset: -80, // se tiver header fixo
+          duration: 1,
+          easing: (t) => 1 - Math.pow(1 - t, 3),
+        });
+      },
+    },
+  );
+};
